816c40b65dd58336b3c53c9be19ecafb5aeda150
Angelo Machils <angelo@machils.org>
2022-12-02 13:25:01 +0200

    Renamed to the correct file extension.

---
+++ Task 2/command_history_t2.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE AMT2;
+USE AMT2;
+CREATE TABLE user (
+	user_id BIGINT SIGNED,
+	username VARCHAR(15),
+	password VARCHAR(20),
+	last_login TIMESTAMP
+);
+CREATE TABLE user_message (
+	user_id INT,
+	createdAt TIMESTAMP,
+	message VARCHAR(1),
+    last_login TIMESTAMP,
+	updatedAt TIMESTAMP
+);
